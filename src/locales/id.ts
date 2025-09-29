9dd393f87f717d4928417ea764fd5122f0f009be
sulenq <fatwalinovera@gmail.com>
2025-09-29 21:49:19 +0700

    styling

--- src/locales/id.ts
+++ src/locales/id.ts
@@ -145,7 +145,7 @@ const translations = {
   "get": "Dapatkan",
   "hide": "Sembunyikan",
   "info_timezone_auto": {
-    "title": "Zona waktu otomatis diset",
+    "title": "Zona waktu diatur secara otomatis",
     "description": "${timezone}"
   },
   "january": "Januari",
