c3a119bfd3ea6b35b5bb28a4a618ca63938ae5da
sulenq <fatwalinovera@gmail.com>
2025-10-02 09:41:25 +0700

    styling

--- src/components/widget/TableSkeleton.tsx
+++ src/components/widget/TableSkeleton.tsx
@@ -12,7 +12,7 @@ export const TableSkeleton = (props: StackProps) => {
   const { themeConfig } = useThemeConfig();
 
   return (
-    <CContainer p={4} pt={2} flex={1} {...restProps}>
+    <CContainer p={3} pt={2} flex={1} {...restProps}>
       <Skeleton flex={1} rounded={themeConfig.radii.container} />
     </CContainer>
   );
