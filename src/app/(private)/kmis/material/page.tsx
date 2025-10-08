96ee21d9b7443586944eef482ebcc26b9695db9f
sulenq <fatwalinovera@gmail.com>
2025-10-08 10:48:20 +0700

    refactor

--- src/app/(private)/kmis/material/page.tsx
+++ src/app/(private)/kmis/material/page.tsx
@@ -175,6 +175,8 @@ const MaterialFormByType = (props: any) => {
           <FileInput
             dropzone
             maxFiles={5}
+            accept="application/pdf,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.ms-powerpoint,application/vnd.openxmlformats-officedocument.presentationml.presentation"
+            acceptPlaceholder=".pdf, .doc, .docx, .xls, .xlsx, .ppt, .pptx"
             inputValue={formik.values.materialFiles}
             onChange={(inputValue) => {
               formik.setFieldValue("materialFiles", inputValue);
