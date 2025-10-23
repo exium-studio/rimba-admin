cd819ba01457a4c7d8932c07c501e406b9a88e5a
sulenq <fatwalinovera@gmail.com>
2025-10-23 20:36:47 +0700

    refactor: fix reset password func

--- src/components/widget/ResetPasswordDisclosure.tsx
+++ src/components/widget/ResetPasswordDisclosure.tsx
@@ -29,7 +29,6 @@ import { HelperText } from "../ui/helper-text";
 import { PasswordInput } from "../ui/password-input";
 import { StringInput } from "../ui/string-input";
 import BackButton from "./BackButton";
-import { disclosureId } from "@/utils/disclosure";
 
 const Step1 = (props: any) => {
   // Props
@@ -42,13 +41,6 @@ const Step1 = (props: any) => {
   // Hooks
   const { req, loading } = useRequest({
     id: "forgot_password",
-    errorMessage: {
-      400: {
-        INVALID_EMAIL: {
-          ...l.error_invalid_email,
-        },
-      },
-    },
   });
 
   // States
@@ -86,7 +78,7 @@ const Step1 = (props: any) => {
     <>
       <DisclosureBody>
         <CContainer>
-          <form>
+          <form id="reset_password_step_1" onSubmit={formik.handleSubmit}>
             <Field
               label="Email"
               invalid={!!formik.errors.email}
@@ -112,8 +104,9 @@ const Step1 = (props: any) => {
         <BackButton />
 
         <Btn
+          type="submit"
+          form="reset_password_step_1"
           colorPalette={themeConfig.colorPalette}
-          onClick={formik.submitForm}
           loading={loading}
         >
           {l.recieve} OTP
@@ -141,13 +134,14 @@ const Step2 = (props: any) => {
     initialValues: { email: email, otp: "" },
     validationSchema: yup.object().shape({
       email: yup.string().email().required(l.msg_required_form),
-      otp: yup.string().email().required(l.msg_required_form),
+      otp: yup.string().required(l.msg_required_form),
     }),
     onSubmit: (values) => {
       // console.log(values);
 
       const payload = new FormData();
       payload.append("email", values.email);
+      payload.append("otp", values.otp);
       const url = `/api/verify-otp`;
       const config = {
         url,
@@ -171,7 +165,7 @@ const Step2 = (props: any) => {
     <>
       <DisclosureBody>
         <CContainer>
-          <form>
+          <form id="reset_password_step_2" onSubmit={formik.handleSubmit}>
             <Field
               label="OTP"
               invalid={!!formik.errors.otp}
@@ -183,7 +177,7 @@ const Step2 = (props: any) => {
                 w={"full"}
                 size={"xl"}
                 onValueChange={(e) => {
-                  formik?.setFieldValue("otp", e.value);
+                  formik?.setFieldValue("otp", e.value.join(""));
                 }}
               >
                 <PinInputHiddenInput />
@@ -216,8 +210,9 @@ const Step2 = (props: any) => {
         <BackButton />
 
         <Btn
+          type="submit"
+          form="reset_password_step_2"
           colorPalette={themeConfig.colorPalette}
-          onClick={formik.submitForm}
           loading={loading}
         >
           {l.verify} OTP
@@ -267,7 +262,7 @@ const Step3 = (props: any) => {
       payload.append("email", values.email);
       payload.append("otp", values.otp);
       payload.append("password", values.password);
-      payload.append("password_confirmation", values.password_confirmation);
+      payload.append("passwordConfirmation", values.password_confirmation);
       const url = `/api/reset-password`;
       const config = {
         url,
@@ -290,7 +285,7 @@ const Step3 = (props: any) => {
     <>
       <DisclosureBody>
         <CContainer>
-          <form>
+          <form id="reset_password_step_3" onSubmit={formik.handleSubmit}>
             <Field
               label={"Password"}
               invalid={!!formik.errors.password}
@@ -328,8 +323,9 @@ const Step3 = (props: any) => {
         <BackButton />
 
         <Btn
+          type="submit"
+          form="reset_password_step_3"
           colorPalette={themeConfig.colorPalette}
-          onClick={formik.submitForm}
           loading={loading}
           disabled={
             !!!(formik.values.password && formik.values.password_confirmation)
@@ -348,7 +344,7 @@ const ResetPasswordDisclosure = (props: any) => {
 
   // Hooks
   const { open, onOpen, onClose } = useDisclosure();
-  useBackOnClose(disclosureId(`reset-password`), open, onOpen, onClose);
+  useBackOnClose(`reset-password`, open, onOpen, onClose);
 
   // States
   const [otp, setOtp] = useState<string>("");
